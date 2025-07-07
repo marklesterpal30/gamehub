107f4aa2e31ac6f20e06c8e2a28431feee1f1b74
Mark Lester <marklestermarananpal@gmail.com>
2025-07-07 12:25:14 +0800

    Displaying critic score

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -9,7 +9,7 @@ const GameGrid = () => {
 			{error && <Text>{error}</Text>}
 			<SimpleGrid
 				padding={10}
-				columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+				columns={{ sm: 1, md: 2, lg: 3 }}
 				spacing={10}
 			>
 				{games.map((game) => (
